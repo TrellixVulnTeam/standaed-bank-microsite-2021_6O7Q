8d668b829d664fa944d6e8f3a286737d1e62fd8b
jose-candido <jose.candido@pt.ey.com>
2022-04-05 22:38:06 +0100

    Desktop/Navbar - Color when menu open

--- js/menuOpen.js
+++ js/menuOpen.js
@@ -3,18 +3,32 @@ function menuFunction() {
     const rolling = document.querySelector(".stop-scrolling");
     const buttonActive = document.querySelector(".navbar__button--2");
     const buttonActiveMobile = document.querySelector(".navbar__button--2-mobile");
-    const activeButton = document.querySelector(".active-button");
+    const ActiveHamb = document.querySelectorAll(".hamburguer-icon__bar");
+    const ActiveDown = document.querySelectorAll(".downloadText");
     const button1 = document.querySelector(".navbar__button--1");
     if (x.className.includes('menu-in')) {
       x.classList.add('menu-out');
       x.classList.remove('menu-in');
       rolling.style.overflowY = "auto";
+      for (const box of ActiveHamb) {
+        box.classList.remove('hamburguer-icon__active');
+      }
+      for (const box of ActiveDown) {
+        box.classList.remove('navbar-downloadTextOn');
+      }
       buttonActive.classList.remove('button-active');
       buttonActiveMobile.classList.remove('button-active-mobile');
       button1.classList.remove('remove-border');
     } else {
       x.classList.remove('menu-out');
       x.classList.add('menu-in');
+      for (const box of ActiveHamb) {
+        box.classList.add('hamburguer-icon__active');
+      }
+      for (const box of ActiveDown) {
+        box.classList.add('navbar-downloadTextOn');
+      }
+      // ActiveHamb.classList.add('hamburguer-icon__active')
       rolling.style.overflowY = "hidden";
       buttonActive.classList.add('button-active');
       buttonActiveMobile.classList.add('button-active-mobile');
