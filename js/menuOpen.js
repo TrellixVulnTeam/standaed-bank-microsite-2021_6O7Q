e78fcaa84a2a8538565fd29080b7bdf4584f7f49
jose-candido <jose.candido@pt.ey.com>
2022-04-22 13:02:25 +0100

    Clean files

--- js/menuOpen.js
+++ js/menuOpen.js
@@ -1,11 +1,8 @@
 function menuFunction() {
     var x = document.getElementById("downloads");
     const rolling = document.querySelector(".stop-scrolling");
-    const buttonActive = document.querySelector(".navbar__button--2");
-    const buttonActiveMobile = document.querySelector(".navbar__button--2-mobile");
     const ActiveHamb = document.querySelectorAll(".hamburguer-icon__bar");
     const ActiveDown = document.querySelectorAll(".downloadText");
-    const button1 = document.querySelector(".navbar__button--1");
     if (x.className.includes('menu-in')) {
       x.classList.add('menu-out');
       x.classList.remove('menu-in');
@@ -17,9 +14,6 @@ function menuFunction() {
       for (const box of ActiveDown) {
         box.classList.remove('navbar-downloadTextOn');
       }
-      buttonActive.classList.remove('button-active');
-      buttonActiveMobile.classList.remove('button-active-mobile');
-      button1.classList.remove('remove-border');
       
       
     } else {
@@ -32,11 +26,8 @@ function menuFunction() {
       for (const box of ActiveDown) {
         box.classList.add('navbar-downloadTextOn');
       }
-      // ActiveHamb.classList.add('hamburguer-icon__active')
       rolling.style.overflowY = "hidden";
-      buttonActive.classList.add('button-active');
-      buttonActiveMobile.classList.add('button-active-mobile');
-      button1.classList.add('remove-border');
+
     }
   }
 
